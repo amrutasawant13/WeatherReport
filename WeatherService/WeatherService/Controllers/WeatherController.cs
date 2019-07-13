78e1c5bf98d656a2046d2419db96b275c6628d43
amrutasawant13 <amrutasawantk@gmail.com>
2019-07-13 18:55:21 +0530

    commit-1

---
+++ WeatherService/WeatherService/Controllers/WeatherController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WeatherService.Models;
+
+namespace WeatherService.Controllers
+{
+    public class WeatherController : Controller
+    {
+        static List<CityDetail> cityList = null;
+        public WeatherController()
+        {
+            if (cityList == null)
+            {
+                cityList = GetAllCityIds();
+            }
+        }
+
+        // GET: Weather
+        [HttpGet]
+        public ActionResult GenerateWeatherReport()
+        {
+            if (TempData["SuccessMessage"] != null)
+            {
+                ViewBag.SuccessMessage = TempData["SuccessMessage"];
+            }
+            if (TempData["ErrorMessage"] != null)
+            {
+                ViewBag.ErrorMessage = TempData["ErrorMessage"];
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GenerateWeatherReport(HttpPostedFileBase file)
+        {
+            var supportedTypes = new[] { "csv" };
+            var fileExt = System.IO.Path.GetExtension(file.FileName).Substring(1);
+            if (!supportedTypes.Contains(fileExt))
+            {
+                TempData["ErrorMessage"] = "File Extension Is InValid - Only .csv file allowed";
+            }
+            else
+            {
+                BinaryReader b = new BinaryReader(file.InputStream);
+                byte[] binData = b.ReadBytes(file.ContentLength);
+                string result = System.Text.Encoding.UTF8.GetString(binData);
+                string[] cities = result.Split(',');
+
+                var givenCityDetails = from c1 in cityList
+                                       join c2 in cities
+                                       on c1.name equals c2
+                                       select c1;
+
+                foreach (var city in givenCityDetails)
+                {
+                    GenerateWeatherReportForToday(city);
+                }
+
+                TempData["SuccessMessage"] = "Weather report has been generated for the uploaded cities.";
+            }
+
+            return RedirectToAction("GenerateWeatherReport");
+        }
+
+        private List<CityDetail> GetAllCityIds()
+        {
+            FileStream fileStream = new FileStream(String.Format("{0}\\city.list.json", AppDomain.CurrentDomain.BaseDirectory), FileMode.Open, FileAccess.Read);
+            BinaryReader b = new BinaryReader(fileStream);
+            byte[] binData = b.ReadBytes((int)fileStream.Length);
+            string cityListString = System.Text.Encoding.UTF8.GetString(binData);
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            serializer.MaxJsonLength = Int32.MaxValue;
+            List<CityDetail> cityDetails = serializer.Deserialize<List<CityDetail>>(cityListString);
+            return cityDetails;
+        }
+
+        private void GenerateWeatherReportForToday(CityDetail city)
+        {
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("http://api.openweathermap.org")
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, string.Format("/data/2.5/weather?id={0}&APPID={1}", city.id, "ef4738f1ce56b1c3ad758a2b1024a2a2"));
+            HttpResponseMessage httpResponseMessage = client.SendAsync(request).Result;
+            string output = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+            var path = String.Format("{0}WeatherReport\\city_{1},{2}_{3}.txt", AppDomain.CurrentDomain.BaseDirectory, city.name, city.country, DateTime.Today.ToString("yyyyMMdd"));
+            System.IO.File.WriteAllText(path, output);
+        }
+    }
+}
---
+++ WeatherService/WeatherService/Models/CityDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherService.Models
+{
+    public class CityDetail
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public Coord coord { get; set; }
+    }
+
+    public class Coord
+    {
+        public decimal lon { get; set; }
+        public decimal lat { get; set; }
+    }
+}
